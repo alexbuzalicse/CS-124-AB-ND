97c11923c8ef1bb1b29cf4b511b0826061d06f2d
n-dohrmann <noah@dohrmann.org>
2024-02-12 10:46:28 -0500

    changed randmst namespaces
    Signed-off-by: n-dohrmann <noah@dohrmann.org>

--- P1/randmst.cpp
+++ P1/randmst.cpp
@@ -1,48 +1,50 @@
 #include <iostream>
 #include <sstream>
 #include <random>
+#include<vector>
 
-float euclideanDistance(std::vector<float> x, std::vector<float> y) {
 
-    float sum = 0;
+using std::vector;
+using std::cout;
+using std::endl;
+using std::default_random_engine;
+using std::uniform_real_distribution;
+using std::stoi;
 
+double euclideanDistance(const vector<double> &x, const vector<double> &y) {
+    double sum = 0;
+    assert (x.size() == y.size());
     for (int i = 0; i < x.size(); i++) {
         sum += (x[i]-y[i])*(x[i]-y[i]);
     }
-
     return sqrt(sum);
 }
 
 int main(int argc, char* argv[])
 {
-
     // Get command line arguments
-    int flexibleFlag = std::stoi(argv[1]);
-    int n = std::stoi(argv[2]);
+    int flexibleFlag = stoi(argv[1]);
+    int n = stoi(argv[2]);
     int m = 65536;
-    int trials = std::stoi(argv[3]);
-    int dimension = std::stoi(argv[4]);
+    int trials = stoi(argv[3]);
+    int dimension = stoi(argv[4]);
 
     // Declare variables
-    std::default_random_engine generator;
-    std::uniform_real_distribution<double> distribution(0.0,1.0);
-    std::vector<std::vector<float>> graph (n, std::vector<float>(dimension));
+    default_random_engine generator;
+    uniform_real_distribution<double> distribution(0.0,1.0);
+    vector<vector<double>> graph (n, vector<double>(dimension));
 
     // Random edge weight (dimension = 0)
     if (dimension == 0) {
-        
-        std::vector<std::vector<float>> adjacencyMatrix (n, std::vector<float>(n));
+        vector<vector<double>> adjacencyMatrix (n, vector<double>(n));
         for (int i = 0; i < n; i++) {
             for(int j = i; j < n; j++) {
                 adjacencyMatrix[i][j] = distribution(generator);
                 adjacencyMatrix[j][i] = adjacencyMatrix[i][j];
             }
         }
-
     }
-    
     else {
-
         // Generate graph, each row is a vertex with a point in (dimension) dimensions
         for(int i = 0; i < n; i++) {
             for(int j = 0; j < dimension; j++) {
@@ -51,7 +53,7 @@ int main(int argc, char* argv[])
         }
 
         // Get adjacency matrix
-        std::vector<std::vector<float>> adjacencyMatrix (n, std::vector<float>(n));
+        vector<vector<double>> adjacencyMatrix (n, vector<double>(n));
         for (int i = 0; i < n; i++) {
             for(int j = 0; j < n; j++) {
                 adjacencyMatrix[i][j] = euclideanDistance(graph[i], graph[j]);
