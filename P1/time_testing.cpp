08a55f67c7c66fae8bb8fa75f2cc4ce5a508eeec
n-dohrmann <noah@dohrmann.org>
2024-02-20 12:13:07 -0500

    fixing time test file
    Signed-off-by: n-dohrmann <noah@dohrmann.org>

--- P1/time_testing.cpp
+++ P1/time_testing.cpp
@@ -1,17 +1,19 @@
 #include"algos.hpp"
 
-void gen_vertices(int n, int dim) {
-    vector<vector<int>> a(n, vector<int>(4));
+vector<vector<double>> gen_vertices(int n, int dim) {
+    vector<vector<double>> verts(n, vector<double>(4));
     for (int i = 0; i < n; ++i) {
         for (int j = 0; j < dim; ++j) {
-            a[i][j] = uniformDistribution(generator);
+            verts[i][j] = uniformDistribution(generator);
         }
     }
+    return verts;
 }
 
 int main() {
     int n = 260000;
     int dim = 4;
-    gen_vertices(n, dim);
+    vector<vector<double>> verts = gen_vertices(n, dim);
+    cout << verts[n-1][dim-1] << endl;
     return 0;
 }
