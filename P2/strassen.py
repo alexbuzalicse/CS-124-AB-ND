d3cddb6b621df85b8d8919ec9eb1d6025f9227c9
alexbuzalicse <alexbuzali@g.harvard.edu>
2024-03-20 21:00:33 -0400

    Strassen

--- P2/strassen.py
+++ P2/strassen.py
@@ -25,18 +25,62 @@ def get_k(n : int) -> int:
     """ return the padded size k from an input size n """
     return 2 ** np.ceil(np.log2(n))
 
-def strassen_multiply(A : np.ndarray, B : np.ndarray, n_0 : int) -> np.ndarray:
+def strassen_multiply(U : np.ndarray, V : np.ndarray, n_0 : int) -> np.ndarray:
     """
     Takes in two numpy arrays A and B, and a cutoff point n_0. Performs Strassen's
-    algorithm for matrix multiplication until the size of the recurive matrices is n_0.
+    algorithm for matrix multiplication until the size of the recursive matrices is n_0.
     At this point, conventional matrix multiplication is used. Returns the computed product
     C = A @ B
     """
-    C = np.zeros(shape=A.shape)
 
-    # TODO - implement Strassen's with cutoff point n_0
+    n = U.shape[0]
 
-    return C
+    # Cutoff point
+    if n <= n_0:
+        return U@V
+
+    # Pad arrays if necessary
+    if n % 2 == 1:
+
+        X = np.zeros((n+1,n+1))
+        X[:n,:n] = U
+
+        Y = np.zeros((n+1,n+1))
+        Y[:n,:n] = V
+
+        nPrime = n+1
+
+    else:
+        X = U
+        Y = V
+        nPrime = n
+
+    A = X[:nPrime//2,:nPrime//2]
+    B = X[:nPrime//2,nPrime//2:]
+    C = X[nPrime//2:,:nPrime//2]
+    D = X[nPrime//2:,nPrime//2:]
+
+    E = Y[:nPrime//2,:nPrime//2]
+    F = Y[:nPrime//2,nPrime//2:]
+    G = Y[nPrime//2:,:nPrime//2]
+    H = Y[nPrime//2:,nPrime//2:]
+
+    P1 = strassen_multiply(A,F-H,n_0)
+    P2 = strassen_multiply(A+B,H,n_0)
+    P3 = strassen_multiply(C+D,E,n_0)
+    P4 = strassen_multiply(D,G-E,n_0)
+    P5 = strassen_multiply(A+D,E+H,n_0)
+    P6 = strassen_multiply(B-D,G+H,n_0)
+    P7 = strassen_multiply(C-A,E+F,n_0)
+
+    Z1 = -P2 + P4 + P5 + P6
+    Z2 = P1 + P2
+    Z3 = P3 + P4
+    Z4 = P1 - P3 + P5 +P7
+
+    Z = np.block([[Z1,Z2],[Z3,Z4]])
+
+    return Z[:n,:n]
 
 def main():
     # size of matrix
