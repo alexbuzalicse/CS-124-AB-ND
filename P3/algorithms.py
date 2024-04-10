e773de0ead3212bb97b741269fca6b3dacc9b4e7
alexbuzalicse <alexbuzali@g.harvard.edu>
2024-04-10 19:24:25 -0400

    Initial Push

---
+++ P3/algorithms.py
@@ -0,0 +1,77 @@
+import numpy as np
+
+def residue(A, S):
+    return np.abs(A@S)
+
+def T(iteration):
+    return (10**10) * (0.8**np.floor(iteration/300))
+
+def randomNeighbor(S, n):
+
+    sPrime = np.copy(S)
+    i,j = np.random.choice(range(n),2,False)
+    sPrime[i] *= -1
+    sPrime[j] = np.random.choice([sPrime[j], -sPrime[j]])
+    return sPrime
+
+def karmarkarKarp(A):
+
+    Acopy = np.copy(A)
+
+    for iteration in range(len(A)):
+        i = np.argpartition(Acopy,-2)[-2:]
+        Acopy[i[1]] = Acopy[i[1]] - Acopy[i[0]]
+        Acopy[i[0]] = 0
+
+    return np.partition(Acopy,-1)[-1]
+
+def repeatedRandom(A, maxIterations):
+
+    n = len(A)
+    S = np.random.choice([-1,1],n)
+
+    for iteration in range(maxIterations):
+        sPrime = np.random.choice([-1,1],n)
+        if residue(A,sPrime) < residue(A,S):
+            S = sPrime
+    return S
+
+def hillClimbing(A, maxIterations):
+
+    n = len(A)
+    S = np.random.choice([-1,1],n)
+
+    for iteration in range(maxIterations):
+
+        sPrime = randomNeighbor(S,n)
+
+        if residue(A,sPrime) < residue(A,S):
+            S = sPrime
+
+    return S
+
+def simulatedAnnealing(A, maxIterations):
+
+    n = len(A)
+    S = np.random.choice([-1,1],n)
+    sPrimePrime = np.copy(S)
+
+    for iteration in range(maxIterations):
+
+        sPrime = randomNeighbor(S,n)
+        resS = residue(A,S)
+        resSPrime = residue(A, sPrime)
+
+        if resSPrime < resS:
+            S = sPrime
+
+        else:
+            prob = np.exp(-(resSPrime-resS)/T(iteration))
+
+            choice = np.random.choice([0,1],p=[prob,1-prob])
+            if choice == 0: S = sPrime
+
+        if residue(A,S) < residue(A,sPrimePrime):
+            sPrimePrime = S
+
+    return sPrimePrime
---
+++ P3/partition.py
@@ -0,0 +1,11 @@
+import sys
+
+def main():
+
+    flag = int(sys.argv[1])
+    algorithm = int(sys.argv[2])
+    inputfile = int(sys.argv[3])
+
+
+if __name__ == "__main__":
+    main()
